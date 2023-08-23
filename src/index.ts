ae83466cad42b43b5507b07f621faea87068024c
Vitaliy Pasiuta <vpasiuta@griddynamics.com>
2023-08-23 18:00:08 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
+interface Handlers{
+  next: Function,
+  error:Function,
+  complete: Function,
+}
+
+interface User{
+  name: string,
+  age: number,
+  roles: string[],
+  createdAt: Date,
+  isDeleated: boolean,
+}
+
 class Observer {
-  constructor(handlers) {
+  handlers: Handlers;
+  isUnsubscribed: boolean;
+  _unsubscribe: Function;
+
+  constructor(handlers: Handlers) {
     this.handlers = handlers;
     this.isUnsubscribed = false;
-    this._unsubscribe = undefined;
   }
 
-  next(value) {
+  next(value: Observer): void {
     if (this.handlers.next && !this.isUnsubscribed) {
       this.handlers.next(value);
     }
   }
 
-  error(error) {
+  error(error): void {
     if (!this.isUnsubscribed) {
       if (this.handlers.error) {
         this.handlers.error(error);
@@ -21,7 +38,7 @@ class Observer {
     }
   }
 
-  complete() {
+  complete(): void {
     if (!this.isUnsubscribed) {
       if (this.handlers.complete) {
         this.handlers.complete();
@@ -31,7 +48,7 @@ class Observer {
     }
   }
 
-  unsubscribe() {
+  unsubscribe(): void {
     this.isUnsubscribed = true;
 
 if (this._unsubscribe) {
@@ -41,24 +58,26 @@ if (this._unsubscribe) {
 }
 
 class Observable {
+  _subscribe: Function;
+
   constructor(subscribe) {
     this._subscribe = subscribe;
   }
 
   static from(values) {
-    return new Observable((observer) => {
+    return new Observable((observer: Observer): Function => {
       values.forEach((value) => observer.next(value));
 
       observer.complete();
 
-      return () => {
+      return (): void => {
         console.log('unsubscribed');
       };
     });
   }
 
-  subscribe(obs) {
-    const observer = new Observer(obs);
+  subscribe(obs): Object {
+    const observer: Observer = new Observer(obs);
 
     observer._unsubscribe = this._subscribe(observer);
 
@@ -70,14 +89,17 @@ class Observable {
   }
 }
 
-const HTTP_POST_METHOD = 'POST';
-const HTTP_GET_METHOD = 'GET';
+type HTTP_METHODS = 'POST' | 'GET';
+type HTTP_STATUS = 200 | 500;
 
-const HTTP_STATUS_OK = 200;
-const HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
+const HTTP_POST_METHOD: HTTP_METHODS = 'POST';
+const HTTP_GET_METHOD: HTTP_METHODS = 'GET';
+
+const HTTP_STATUS_OK: HTTP_STATUS = 200;
+const HTTP_STATUS_INTERNAL_SERVER_ERROR: HTTP_STATUS = 500;
 
 
-const userMock = {
+const userMock: User = {
   name: 'User Name',
   age: 26,
   roles: [
@@ -88,7 +110,13 @@ const userMock = {
   isDeleated: false,
 };
 
-const requestsMock = [
+const requestsMock: {
+  method: HTTP_METHODS,
+  host: string,
+  path: string,
+  body?: object,
+  params: object,
+}[] = [
   {
     method: HTTP_POST_METHOD,
     host: 'service.example',
@@ -106,16 +134,18 @@ const requestsMock = [
   }
 ];
 
-const handleRequest = (request) => {
+type RESPONSE = {status: HTTP_STATUS};
+
+const handleRequest = (request): RESPONSE => {
   // handling of request
   return {status: HTTP_STATUS_OK};
 };
-const handleError = (error) => {
+const handleError = (error): RESPONSE => {
   // handling of error
   return {status: HTTP_STATUS_INTERNAL_SERVER_ERROR};
 };
 
-const handleComplete = () => console.log('complete');
+const handleComplete = (): void => console.log('complete');
 
 const requests$ = Observable.from(requestsMock);
 
